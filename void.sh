801de1ce6a6ec10c2b89b0bd063333fcd7d36dd1
um.hau <um.hau@outlook.com>
2020-02-21 23:45:43 +0000

    Update void.sh

--- void.sh
+++ void.sh
@@ -56,12 +56,14 @@ GB()                     { sudo rsync -ah --info=progress2 $UR@$IP:"$1" "$1"; }
 
 # also: smbd udevd rpcbind dhcpcd cupsd
 # sudo ln -s /etc/sv/sshd /var/service/                                   # ssh
-sudo ln -s /etc/sv/acpid /var/service/            # acpi to get suspend working
+# sudo ln -s /etc/sv/acpid /var/service/          # acpi to get suspend working
 sudo ln -s /etc/sv/cupsd /var/service/                    # print server client
 sudo ln -s /etc/sv/nfs-server /var/service/                               # nfs
 
 
 ## NOTES ## ---------------------------------------------------------------- ##
+
+echo "done."
 
 # modify acpi actions (lid close, power button, etc.) with /etc/acpi/handler.sh
 
