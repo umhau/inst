01081b1cf7fc887173829bd51a5944d014df0b4e
um.hau <um.hau@outlook.com>
2020-09-04 19:13:00 +0000

    cleaned up the formatting and clarified some stuff

--- tools/create_offline_apt_package_repository.sh
+++ tools/create_offline_apt_package_repository.sh
@@ -4,12 +4,21 @@ set -ev
 # based on https://umhau.github.io/offline-package-installation-with-local-apt-repository/
 # create an offline repo. the output of this script is a .tar file that can be downloaded and moved to the offline machine.
 
-pkgs=(banshee vlc emacs )
-fnm="/home/`whoami`/myrepository" && mkdir $fnm
-for PKG in "${pkgs[@]}"; do sudo apt -d install $PKG -y; sudo cp -n /var/cache/apt/archives/*.deb "$fnm/"; sudo apt clean; done
+repository_name="repo-`date +"%Y-%m-%d"`"
+
+pathname="/home/`whoami`/$repository_name" && mkdir -p $pathname
+
+source ./offline_package_list.sh
+
+for PKG in "${PK[@]}"
+do
+  sudo apt -d install $PKG -y
+  sudo cp -n /var/cache/apt/archives/*.deb "$pathname/"
+  sudo apt clean
+done
 
 sudo apt-get install dpkg-dev -y
-cd /home/`whoami`/myrepository
+cd /home/`whoami`/$repository_name
 dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
 
-cd; tar cfv "$fnm".tar $fnm
+cd ..; tar cfv "$repository_name".tar $pathname
