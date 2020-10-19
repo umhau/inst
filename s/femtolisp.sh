f9c41fc16532b854dae372b81b5204967e97e2bb
um.hau <um.hau@outlook.com>
2020-10-19 17:09:37 +0000

    Create femtolisp.sh

---
+++ s/femtolisp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# The parser for Julia is implemented in Femtolisp, a dialect of Scheme (Julia is inspired by Scheme, which in turn is a Lisp dialect). 
+
+git clone https://github.com/JeffBezanson/femtolisp.git
