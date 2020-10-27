f6982ae7dd00b8dd7e8fefffa71b4702ec3f89e9
um.hau <um.hau@outlook.com>
2020-10-27 00:47:51 +0000

    Update installpkgs.ubuntu.sh

--- s/installpkgs.ubuntu.sh
+++ s/installpkgs.ubuntu.sh
@@ -2,6 +2,7 @@ sudo apt --purge autoremove -y
 sudo apt update
 sudo apt upgrade -y
 
+echo "installing packages: $PK"
 for pkg in "${PK[@]}";do sudo apt install -y "$pkg";done
 
 sudo apt --purge autoremove -y
